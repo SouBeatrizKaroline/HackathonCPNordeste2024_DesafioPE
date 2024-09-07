b7d1634c8d8429bbc071389a0d102a49a3dcd330
MateusGaldinoLG <mateuslguilherme@gmail.com>
2024-09-07 09:01:13 -0300

    fix: corrige erro causado por Router

--- hacka-front/src/App.tsx
+++ hacka-front/src/App.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes } from "react-router-dom"
 import { HomePage } from "./app/home/home";
 import { AboutPage } from "./app/sobre/sobre";
 import { SaudePage } from "./app/saude/saude";
@@ -39,5 +39,17 @@ const router = createBrowserRouter([
 ])
 
 export const App = () => {
-  return <RouterProvider router={router} fallbackElement={<p>Loading...</p>}/>;
+  return (
+    <BrowserRouter basename="https://soubeatrizkaroline.github.io/HackathonCPNordeste2024_DesafioPE">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/saude" element={<SaudePage />} />
+          <Route path="/seguranca" element={<SegurancaPage />} />
+          <Route path="/educacao" element={<EducacaoPage />} />
+          <Route path="/emprego" element={<EmpregoPage />} />
+          <Route path="/empreendedorismo" element={<EmpreendedorismoPage />} />
+      </Routes>
+    </BrowserRouter >
+  );
 }
